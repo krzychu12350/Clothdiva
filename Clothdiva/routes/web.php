ae1475298287342e3056d73ffe9ff3bf6f7d7c7d
Krzysztof Karaś <krzychu12350@interia.pl>
2021-04-18 19:28:43 +0200

    migratev1

--- Clothdiva/routes/web.php
+++ Clothdiva/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test', function(){
+    $products=DB::select('select * from products');
+    return $products;
+});
