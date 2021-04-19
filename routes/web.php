070f0522af923da03fe89331bdabbfd551f9fbb6
PatrycjaG2000 <gawe.patrycja@interia.pl>
2021-04-19 15:01:45 +0200

    webmodifyv4

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test', function(){
-    $products=DB::select('select * from help');
+    $products=DB::select('select * from categories');
     return $products;
 });
